39b5442b00ed5f05859d559f5638d117d73bb994
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-03-18 17:09:10 +0300

    Add project files.

---
+++ OrganicVegetables/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrganicVegetables.Models.Entity;
+
+namespace OrganicVegetables.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        TarımDbEntities db=new TarımDbEntities();
+        public ActionResult Index()
+        {
+            var values = db.TBLURUNLER.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ OrganicVegetables/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OrganicVegetables.MvcApplication" Language="C#" %>
